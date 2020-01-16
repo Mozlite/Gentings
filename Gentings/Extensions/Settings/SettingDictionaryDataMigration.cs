07af61f7adf81b30c47cee991b9bfc92cb1b3c73
ztang <0.369874125>
2020-01-16 15:08:04 +0800

    字典配置。

--- Gentings/Extensions/Settings/SettingDictionaryDataMigration.cs
+++ Gentings/Extensions/Settings/SettingDictionaryDataMigration.cs
@@ -5,7 +5,7 @@ namespace Gentings.Extensions.Settings
     /// <summary>
     /// 数据库迁移。
     /// </summary>
-    public class SettingsDataMigration : DataMigration
+    internal class SettingDictionaryDataMigration : DataMigration
     {
         /// <summary>
         /// 当模型建立时候构建的表格实例。
@@ -13,10 +13,8 @@ namespace Gentings.Extensions.Settings
         /// <param name="builder">迁移实例对象。</param>
         public override void Create(MigrationBuilder builder)
         {
-            builder.CreateTable<SettingsAdapter>(table => table
-                .Column(s => s.SettingKey)
-                .Column(s => s.SettingValue)
-            );
+            builder.CreateTable<SettingDictionary>(table =>
+                table.Column(x => x.Id).Column(x => x.ParentId).Column(x => x.Name).Column(x => x.Value));
         }
     }
 }
